f83a6c06147c921a3d3019f5b4a374f01a4f6be4
Nic <nicole.dun@gmail.com>
2011-03-27 15:52:15 -0700

    added mongo stuff to createGamge

--- gameServlets.js
+++ gameServlets.js
@@ -4,7 +4,12 @@ var http = require('http'),
       fs = require('fs'),
     path = require('path'),
   events = require('events'),
-      io = require('socket.io');
+      io = require('socket.io'), 
+	  Db = require('./lib/mongodb').Db,
+Connection = require('./lib/mongodb').Connection,
+Server = require('./lib/mongodb').Server,
+BSON = require('./lib/mongodb').BSONNative;
+
 
 
 var gameServlets = exports;
@@ -36,7 +41,23 @@ gameServlets.createGame = function(request, response, state){
         state.makeGame(params['query']['gameName']);
         state.registerUser(params['query']['userName'], params['query']['gameName']);
     }
-    
+	var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+	var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+
+    var db = new Db('banana', new Server(host, port, {}), {native_parser:true});
+			db.open(function(err, db) {		
+						sys.puts("saving game db");		
+				db.collection('games', function(err, collection) {
+					collection.insert([{'name':params['query']['userName'], 'password': params['query']['gameName']}], function(docs) {
+			          // Count the number of records
+			          collection.count(function(err, count) {
+			            sys.puts("There are " + count + " records.");
+			          });
+			});
+				});	
+				db.close();
+			});
+	
     response.end();
 }
 
